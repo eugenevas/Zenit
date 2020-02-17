d0b836391ac859f96608e81217a7b2bd8fba59c2
Evgeniy Vasilyev <eugene.vas25@gmail.com>
2020-02-17 09:30:43 +0300

    delete some comments

--- Zenit/Controller/PlayerBioViewController.swift
+++ Zenit/Controller/PlayerBioViewController.swift
@@ -27,22 +27,3 @@ class PlayerBioViewController: UIViewController {
     
 }
 
-
-//class CoachBioViewController: UIViewController {
-//
-//    @IBOutlet weak var image: UIImageView!
-//    @IBOutlet weak var name: UILabel!
-//    @IBOutlet weak var bio: UITextView!
-//
-//
-//    var nameCoach = "Just a name"
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        image.image = UIImage(named: nameCoach)
-//        name.text = nameCoach
-//        bio.text = "Here will be text"
-//
-//    }
-//}
